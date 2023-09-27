d1d65578a577fcec88d6a5200a33598620cacb7c
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-09-27 15:19:06 -0300

    Update atvd_grafo.cpp

--- atividade_grafo/atvd_grafo.cpp
+++ atividade_grafo/atvd_grafo.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream>
+using namespace std;
 
-using std::cout;
-using std::endl;
+class Graph {
+private:
+    int vertices;
+    vector<vector<int>> adjMatrix;
+    vector<vector<int>> adjList;
 
+public:
+    Graph(int V) : vertices(V) {
+        adjMatrix.resize(V, vector<int>(V, 0));
+        adjList.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    void BFS(int s, int t) {
+        vector<int> parent(vertices, -1);
+        vector<bool> visited(vertices, false);
+
+        queue<int> q;
+        q.push(s);
+        visited[s] = true;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+        }
+        
+        
+        }
+
+}
