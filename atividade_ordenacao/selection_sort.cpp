f5a1cdf034aa88e29ca32d17edf1c2493ca8bf41
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-11-07 13:56:45 -0300

    Update selection_sort.cpp

--- atividade_ordenacao/selection_sort.cpp
+++ atividade_ordenacao/selection_sort.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
+
 
 // Implementação do algoritmo de ordenação selection sort
 
@@ -30,31 +32,61 @@ void selection_sort(vector<int>& v){
         v[min_id] = v[i];
         v[i] = aux;
 
-        cout << "Vetor apos troca " << i + 1 << ":"<<endl;
+        /*cout << "Vetor apos troca " << i + 1 << ":"<<endl;
         cout << v[i]<< " eh o menor e trocou de lugar com " << v[min_id] << endl;
         for(int i=0; i< v.size(); i++){
         cout << "" << v[i] << " ";
         
-        }cout << endl << endl;
+        }*cout << endl << endl;*/
     }
 
 }
 
 
-int main(){
+int main() {
+    std::string file_name = "num.1000.2.txt"; 
+    vector<int> my_vector; // Vamos utilizar vetor
 
-    vector<int> my_vector = {5,1,42,3,7};
+    // Abra o arquivo para leitura
+    std::ifstream file(file_name);
 
+    if (!file.is_open()) {
+        std::cerr << "Nao foi possivel abrir o arquivo." << std::endl;
+        return 1;
+    }
+
+    int number;
+    int count = 0;
+
+
+    while( file >> number){
+        my_vector.push_back(number);
+        count++;
+    }
+
+    int tam = my_vector.size();
+    
     cout << "Vetor original" << endl;
 
-    for(int x=0; x < my_vector.size(); x++){
+    for(int x=0; x < tam; x++){
         cout << "" << my_vector[x] << " ";
     } cout << endl;
-
-    selection_sort(my_vector);
-
-
     
+    
+    
+    selection_sort(my_vector);
+    
+    cout << "Vetor ordenado:" << endl;
+
+    for(int x=0; x < tam; x++){
+        cout << "" << my_vector[x] << " ";
+    } cout << endl;
+    
+
+
+
+    // Feche o arquivo
+    file.close();
 
     return 0;
 }
